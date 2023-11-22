baee6b8e11742ddc6a9c697a3b33d04138578f16
Abdalla <a.eltayeb@campus.tu-berlin.de>
2023-11-22 09:35:59 -0800

    Map sketch

---
+++ IlmFinder/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  IlmFinder
+//
+//  Created by Abdalla Eltayeb on 20.11.23.
+//
+
+import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static let IZDB = CLLocationCoordinate2D(latitude: 42.0, longitude: -71.0)
+    
+    static let Arresalah = CLLocationCoordinate2D(latitude: 42.0, longitude: -73.0)
+}
+
+struct ContentView: View {
+    @State private var selectedTag: Int?
+    var body: some View {
+        VStack{
+            Text("Map")
+            Map(selection: $selectedTag){
+                Marker("IZDB", coordinate: .IZDB)
+                    .tag(1)
+                Marker("Arresalah", coordinate: .Arresalah)
+                    .tag(2)
+            }
+            .mapStyle(.hybrid(elevation: .realistic))
+            .mapControls {
+                MapUserLocationButton()
+                MapCompass()
+                MapScaleView()
+            }
+        }
+       
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IlmFinder/IlmFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  IlmFinderApp.swift
+//  IlmFinder
+//
+//  Created by Abdalla Eltayeb on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct IlmFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
