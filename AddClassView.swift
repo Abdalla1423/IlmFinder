55df3944ec1997d387a705c55588f780edbf2575
IbnHichame <deanbel@berkeley.edu>
2023-11-22 23:51:30 -0800

    added most SwiftUi changes for addClassview

---
+++ AddClassView.swift
@@ -0,0 +1,18 @@
+//
+//  AddClassView.swift
+//  IlmFinder
+//
+//  Created by Salima Fassil on 11/22/23.
+//
+
+import SwiftUI
+
+struct AddClassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddClassView()
+}
--- IlmFinder/ContentView.swift
+++ IlmFinder/ContentView.swift
@@ -32,6 +32,9 @@ struct ContentView: View {
                 MapScaleView()
             }
         }
+        /*VStack {
+            Text("Commented out code to test")
+        }*/
        
         
     }
