de1e9a3cb7c9c017028b462097a9b34ef470ef7b
IbnHichame <deanbel@berkeley.edu>
2023-11-22 23:53:02 -0800

    test

--- AddClassView.swift
+++ AddClassView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct AddClassView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Topic/Title")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+            Rectangle()
+                .fill(.gray.opacity(0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .frame(width: 250, height: 50)
+           
+            Text("Lecturer")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+            Rectangle()
+                .fill(.gray.opacity(0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .frame(width: 250, height: 50)
+               
+            Text("Location")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+            Rectangle()
+                .fill(.gray.opacity(0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .frame(width: 250, height: 50)
+            
+            Spacer()
+            HStack {
+                Text("Add Class").font(.title).bold()
+                
+                
+            }
+            .padding()
+            .background(.blue.opacity(0.1))
+            .clipShape(RoundedRectangle(cornerRadius: 60))
+            .frame(width: 300, height: 30)
+            Spacer()
+            
+        }
     }
 }
 
